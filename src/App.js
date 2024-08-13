8fb418ceb81d3b1631f1e25ec5edaf62833ec8c6
Emerald Dev <emerald.dev@hotmail.com>
2024-08-13 01:18:55 -0400

    final version

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./styles.css"
+import Chat from './Components/Chat'
+
+function App() {
+
+
+  return (
+    <div className="App">
+     <Chat/>
+    </div>
+  );
+}
+
+export default App;
